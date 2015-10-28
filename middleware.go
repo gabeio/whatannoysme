b729e060ab6da3815680665ad47abe18da78d7e4
Gabriel <gabriel@coldoc.co>
2015-10-28 01:55:28 -0400

    adjusted middleware to not use extra variable

--- middleware.go
+++ middleware.go
@@ -5,9 +5,8 @@ import (
 )
 
 func textHtml(h http.Handler) http.Handler {
-	fn := func(w http.ResponseWriter, r *http.Request) {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "text/html")
 		h.ServeHTTP(w, r)
-	}
-	return http.HandlerFunc(fn)
+	})
 }
