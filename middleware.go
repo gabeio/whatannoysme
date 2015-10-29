0d9e8f63c8fefeca655a1055cd65c7d4c18a3cf7
Gabriel <gabriel@coldoc.co>
2015-10-29 01:45:12 -0400

    added charset=utf-8 to textHtml middleware

--- middleware.go
+++ middleware.go
@@ -6,7 +6,7 @@ import (
 
 func textHtml(h http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Type", "text/html")
+		w.Header().Set("Content-Type", "text/html; charset=utf-8")
 		h.ServeHTTP(w, r)
 	})
 }
